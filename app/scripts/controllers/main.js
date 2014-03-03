f6400d36bef69cef011c8791407e45aadf0a1475
Dmitry Manzhasov <dim2man@gmail.com>
2014-03-03 11:14:40 +0400

    initial files from generator-angular

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('csbrowserApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
