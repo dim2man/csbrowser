5c2ae437f10ee9a9cae97e01b0b5ef96ca5d908e
Dmitry Manzhasov <dim2man@gmail.com>
2014-03-06 19:56:43 +0400

    added alert module and contracts table

--- app/scripts/controllers/contracts.js
+++ app/scripts/controllers/contracts.js
@@ -1,27 +1,45 @@
-define(['app', 'exContracts'], function(app, testData) {
+define(['app', 'exContracts', 'util/alert'], function(app, testData, alert) {
   'use strict';
 
   app.module.controller(app.controllers.contracts.name, ['$scope', app.services.clearspending.name,
     function($scope, cs) {
+
       // cs.contracts({
       //   page: 1,
       //   perpage: 20,
-      //   customerregion: 65
+      //   customerregion: 52
       // }).success(parseContracts);
+
+      $scope.columns = [{
+        displayName: 'ID',
+        name: 'id'
+      }, {
+        displayName: 'Reg #',
+        name: 'regNum'
+      }, {
+        displayName: 'Price',
+        name: 'price'
+      }, {
+        displayName: 'Sign Date',
+        name: 'signDate'
+      }, ];
 
       function parseContracts(data) {
         if (typeof data === 'object') {
           data = data.contracts;
         } else {
-          // TODO error
+          alert.err(data);
+          return;
         }
+        alert.info('Received ' + data.data.length + ' records from ' + data.total + ' total');
         $scope.contracts = data;
         $scope.$digest();
       }
 
       setTimeout(parseContracts.bind(this, testData), 200);
 
-      $scope.contracts = 'Receiving contracts';
+      alert.info('Receiving contracts');
+
     }
   ]);
 });
